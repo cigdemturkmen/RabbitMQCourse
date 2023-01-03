57ba00e5585767ad2f5d333176b5220c4f01d30c
Çiğdem Türkmen <cigdemturkmen57@outlook.com>
2023-01-03 13:43:16 +0300

    subscriber/consumer completed

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -4,21 +4,19 @@ using System.Text;
 //My Notes
 // puslisher/producer
 // we normally keep the uri in appsettings.json
-// durable true >>> queues are saved physically. If rabbitmq restarts no queues will be lost. 
-// durable false >>>  queues are kept on memory. If rabbitmq restarts all queues will be lost.
-// exclusive true >>> making a queue exclusive makes is accesible only from the channel it is created.
-// autoDelete true >>> If the last consumer that is connected to the queue is down, the queue will be deleted automatically.
-
-Console.WriteLine("Hello, World!");
+// (queue)durable true >>> queues are saved physically. If rabbitmq restarts no queues will be lost. 
+// (queue)durable false >>>  queues are kept on memory. If rabbitmq restarts all queues will be lost.
+// (queue)exclusive true >>> making a queue exclusive makes is accesible only from the channel it is created.
+// (queue)autoDelete true >>> If the last consumer that is connected to the queue is down, the queue will be deleted automatically.
 
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://bmvwpzdl:oMIUQ5m6Gp7WXOkXwXHFsXF_unEXMC1l@chimpanzee.rmq.cloudamqp.com/bmvwpzdl");
 
-using var connection = factory.CreateConnection();
+using var connection = factory.CreateConnection(); // we can create multiple channels over one connection
 
 var channel = connection.CreateModel(); // We will connect to rabbitMQ through a channel.
 
-channel.QueueDeclare("hello-queue", true, false, false);
+channel.QueueDeclare("hello-queue", true, false, false); // we can create the queue either in producer app or consumer app. If declared on both, QueueDeclare() parameters must be identical on both. otherwise, error will be thrown.
 
 string msg = "hello world";
 
