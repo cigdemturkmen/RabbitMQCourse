1ac38a4e12e33ffefc7d2941ea8f8422e72a587e
Çiğdem Türkmen <cigdemturkmen57@outlook.com>
2023-01-06 10:25:53 +0300

    Multiple messages sent to queue.

--- UdemyRabbitMQ.consumer/Program.cs
+++ UdemyRabbitMQ.consumer/Program.cs
@@ -5,28 +5,34 @@ using System.Text;
 
 //My Notes
 // subscriber/consumer
-// autoAck >>> auto acknowledge: when RabbitMQ ackknowledges that the message is received by the subscriber, the msg in the queue will be deleted automatically if autoAck == true. If false, you have to tell the queue when to delete the msg.
+// BasicConsume autoAck >>> auto acknowledge: when subscriber ackknowledges that it received the msg successfully, the msg in the queue will be deleted automatically if autoAck == true. If false, you have to tell the queue when to delete the msg.
 // In real life, make autoAck false because we dont want the msg to be deleted whether it is processed wrong. We want to make sure if it is processed correctly and then we can delete the msg in the queue.
 // we can create the queue either in producer app or consumer app. If declared on both, QueueDeclare() parameters must be identical on both.
 // otherwise, error will be thrown. If you are sure that the channel is declared in producer. You dont have to declare it here too.
+// BasicQos global >>> if true, X number of msgs means that the total amount of messages send to all of the subscribers are X. If false, each subscriber receives X msgs.
+
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://bmvwpzdl:oMIUQ5m6Gp7WXOkXwXHFsXF_unEXMC1l@chimpanzee.rmq.cloudamqp.com/bmvwpzdl");
 
 using var connection = factory.CreateConnection();
 
-var channel = connection.CreateModel(); // IModel
+var channel = connection.CreateModel();
 
-// channel.QueueDeclare("hello-queue", true, false, false);
+// channel.QueueDeclare("hello-queue", true, false, false); // it is already declared on producer project
+
+channel.BasicQos(0, 1, false); // 0 >>> you can send msgs with any size.
 
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicConsume("hello-queue", true, consumer);
+channel.BasicConsume("hello-queue", false, consumer); // autoAck is false, so we should let know RabbitMQ that message is received successfully
 
 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
     Console.WriteLine("Message: " + message);
+
+    channel.BasicAck(e.DeliveryTag, false);// because we set autoAck to false, we should let know the queue manually like so.
 };
 
 Console.ReadLine();
--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 //My Notes
 // puslisher/producer
 // we normally keep the uri in appsettings.json
+// If we send the msg directly to the queue, it is the default exchange.
 // (queue)durable true >>> queues are saved physically. If rabbitmq restarts no queues will be lost. 
 // (queue)durable false >>>  queues are kept on memory. If rabbitmq restarts all queues will be lost.
 // (queue)exclusive true >>> making a queue exclusive makes is accesible only from the channel it is created.
@@ -18,13 +19,18 @@ var channel = connection.CreateModel(); // We will connect to rabbitMQ through a
 
 channel.QueueDeclare("hello-queue", true, false, false); // we can create the queue either in producer app or consumer app. If declared on both, QueueDeclare() parameters must be identical on both. otherwise, error will be thrown.
 
-string msg = "hello world";
 
-var messageBody = Encoding.UTF8.GetBytes(msg);
+// sending 50 messages to the queue.
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string msg = $"Message {x}";
 
-channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+    var messageBody = Encoding.UTF8.GetBytes(msg);
 
-Console.WriteLine("Message sent");
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+    Console.WriteLine($"Message sent: {msg} ");
+});
 
 Console.ReadLine();
 
